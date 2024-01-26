694abf4c5b44d82fb4e4daccb0b338e8e6bff2d5
dmg04144 <dmg04144@naver.com>
2024-01-26 09:53:40 +0900

    11

---
+++ js/promise.js
@@ -0,0 +1,13 @@
+let test = new Promise((resolve,reject)=>{
+    setTimeout(()=> console.log('비동기 작업 실행'),1000);
+});
+
+test
+.catch(err=> console.log(err))
+.finally(()=>{console.log('작업끝')});
+
+let fetch = () => {
+    return new Promise((resolve,reject)=>{
+
+    })
+}
